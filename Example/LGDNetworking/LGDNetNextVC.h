bb6af5105ad3474349b59b7dc29940737acfca19
刘国栋 <guodong.liu@daddylab.com>
2021-05-13 11:13:57 +0800

    init

---
+++ Example/LGDNetworking/LGDNetNextVC.h
@@ -0,0 +1,17 @@
+//
+//  LGDNetNextVC.h
+//  LGDNetworking_Example
+//
+//  Created by liuguodong on 2021/5/12.
+//  Copyright © 2021 刘国栋. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LGDNetNextVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/LGDNetworking/LGDNetNextVC.m
@@ -0,0 +1,32 @@
+//
+//  LGDNetNextVC.m
+//  LGDNetworking_Example
+//
+//  Created by liuguodong on 2021/5/12.
+//  Copyright © 2021 刘国栋. All rights reserved.
+//
+
+#import "LGDNetNextVC.h"
+
+@interface LGDNetNextVC ()
+
+@end
+
+@implementation LGDNetNextVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
